a0bcc9a8e4e048703028fc5393718968cd2b9437
papsel <elijah.njora@student.moringaschool.com>
2023-07-23 11:03:01 +0300

    completed assignment

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+  useEffect(() => {
+    // Timer function to decrease timeRemaining by 1 every second
+    const timer = setTimeout(() => {
+      setTimeRemaining((prevTime) => prevTime - 1);
+    }, 1000);
+
+    // Cleanup function to clear the timer when the component unmounts or when timeRemaining hits 0
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [timeRemaining]);
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
@@ -11,6 +21,19 @@ function Question({ question, onAnswered }) {
   }
 
   const { id, prompt, answers, correctIndex } = question;
+
+  useEffect(() => {
+    // Reset the timeRemaining to 10 seconds when the question changes
+    setTimeRemaining(10);
+  }, [question]);
+
+  useEffect(() => {
+    // Check if timeRemaining hits 0 and call onAnswered(false) to trigger behavior in the App component
+    if (timeRemaining === 0) {
+      setTimeRemaining(10);
+      onAnswered(false);
+    }
+  }, [timeRemaining, onAnswered]);
 
   return (
     <>
